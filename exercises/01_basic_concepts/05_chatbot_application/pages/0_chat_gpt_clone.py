cc6d538f11b30522298d6eceb2522f978d59dffa
Danu Pranantha <dpranantha@bol.com>
2025-03-06 11:16:44 +0100

    Update

--- exercises/01_basic_concepts/05_chatbot_application/pages/0_chat_gpt_clone.py
+++ exercises/01_basic_concepts/05_chatbot_application/pages/0_chat_gpt_clone.py
@@ -9,8 +9,8 @@ dotenv.load_dotenv()
 # Here we create the client.
 # Make sure you select the LLM provider that corresponds to the one you are using in this course!
 client = instantiate_langchain_model(
-    llm_provider="azure",
-    # llm_provider="gcp",
+    # llm_provider="azure",
+    llm_provider="gcp",
 )
 
 title = "Chatbot App"
@@ -25,6 +25,11 @@ with st.sidebar:
     st.header("Settings")
     # Exercise: add settings for max_tokens, temperature and top_p
     # YOUR CODE HERE START
+    st.sidebar.markdown("# Controls")
+    max_tokens = st.sidebar.slider("Max Tokens", min_value=0, max_value=1000, value=1000)
+    temperature = st.sidebar.slider("Temperature", min_value=0.01, max_value=2.0, value=1.0, step=0.1)
+    top_p = st.sidebar.slider("Top-p", min_value=0.1, max_value=1.0, value=0.5, step=0.1)
+    system_prompt = st.sidebar.text_input(label="System Prompt", value="You are a helpful assistant that help with every day task!")
     # YOUR CODE HERE END
 
 
@@ -56,7 +61,8 @@ if prompt:
             # Exercise: Add a system message that describes the assistant's task.
             # Bonus: make the prompt configurable from the sidebar
             # YOUR CODE HERE START
-            # YOUR CODE HERE END
+            system_prompt
+            # YOUR CODE HERE END,
         ]
         for m in st.session_state.messages:
             messages.append({"role": m["role"], "content": m["content"]})
@@ -65,6 +71,9 @@ if prompt:
             input=messages,
             # Exercise: add max_tokens, temperature and top_p to the completion request
             # YOUR CODE HERE START: test 123
+            max_tokens=max_tokens,
+            temperature=temperature,
+            top_p=top_p
             # YOUR CODE HERE END
         )
         assistant_message = response.content
